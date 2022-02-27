93bb7320ba6c2465dd46682cfd436ea93b243186
efrosparascovia <efros.parascovia@gmail.com>
2022-02-27 23:15:21 +0200

    Lesson 9 corected

--- src/ProgramHelp/Validator.java
+++ src/ProgramHelp/Validator.java
@@ -7,6 +7,7 @@ public class Validator {
     public double validateInput() {
         Scanner scanner = new Scanner(System.in);
         double input = scanner.nextDouble();
+
         while (input <= 0) {
             System.out.println("Negative values or zero are not accepted");
             System.out.println("Enter the value again: ");
--- src/lesson9homework/exercise1/RectangleArea.java
+++ src/lesson9homework/exercise1/RectangleArea.java
@@ -2,17 +2,13 @@ package lesson9homework.exercise1;
 
 import ProgramHelp.Validator;
 
-import java.util.Scanner;
-
 public class RectangleArea {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("The length of rectangle is:");
         Validator validator = new Validator();
         double length = validator.validateInput();
         System.out.println("The width of rectangle is:");
         double width = validator.validateInput();
-        scanner.close();
 
         Area area = new Area();
         area.setDim(length, width);
